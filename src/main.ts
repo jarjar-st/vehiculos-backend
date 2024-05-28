2aa7adcb4c54e3d6eed1b75b65fc36b395a80466
jarjar-st <josue_benjamin12@hotmail.com>
2024-05-27 22:00:11 -0600

    activar_cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Habilita las solicitudes CORS para todos los dominios
   await app.listen(3000);
 }
 bootstrap();
